8d64b33eab2a1f0355c58f7c5a2919a0d5462e57
chauhan-ankit00 <ankitchauhanrps9990@gmail.com>
2025-12-03 18:48:27 +0530

    upload more data

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,12 +1,34 @@
+// import React from "react";
+// import PhotoGallery from "./PhotoGallery";
+// import img1 from "./img/1.png";
+
+// export default function GallerySection() {
+//   const images = [
+//     img1,
+
+//   ];
+
+//   return (
+//     <div id="gallery" className="container-xxl py-6">
+//       <h2
+//         className="text-center mb-5"
+//         style={{ fontFamily: "Roboto", fontWeight: "700", fontSize: "42px" }}
+//       >
+//         Photo Gallery
+//       </h2>
+
+//       <PhotoGallery images={images} />
+//     </div>
+//   );
+// }
 import React from "react";
 import PhotoGallery from "./PhotoGallery";
-import img1 from "./img/1.png";
 
 export default function GallerySection() {
-  const images = [
-    img1,
-
-  ];
+  // Auto-import all images from the /img folder
+  const images = Object.values(
+    import.meta.glob("./img/*.{png,jpg,jpeg,webp}", { eager: true })
+  ).map((img) => img.default);
 
   return (
     <div id="gallery" className="container-xxl py-6">
