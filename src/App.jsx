5a502b530cf31005384f13eac3aa92895f78fba1
chauhan-ankit00 <ankitchauhanrps9990@gmail.com>
2025-12-20 14:15:01 +0530

    more image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import React from 'react'
-import About from './components/About.jsx'
-import Client from './components/Client.jsx'
-import Features from './components/Features.jsx'
-import Footer from './components/Footer.jsx'
 
-import NavbarHero from './components/NavbarHero.jsx'
-import Newsletter from './components/Newsletter.jsx'
-import Service from './components/Service.jsx'
-import ContactForm from './components/ContactForm.jsx'
-import Team from './components/Team.jsx'
-import Testimonial from './components/Testimonial.jsx'
-import Blog from './components/Blog.jsx'
+
 import GallerySection from './components/GallerySection.jsx'
 import SachinGallery from './components/SachinGallery.jsx'
 export default function App() {
@@ -18,23 +7,7 @@ export default function App() {
     <>
     <GallerySection />
     <SachinGallery />
-    {/* <NavbarHero />
-      <About />
-      <Newsletter />
-      <Service />
-      <Features />
-      <Client />
-      
-      
-      
-      
-      <Testimonial />
-      
-      <Team />
-      <Blog />
-      <ContactForm />
-      
-      <Footer /> */}
+   
     </>
   )
 }
